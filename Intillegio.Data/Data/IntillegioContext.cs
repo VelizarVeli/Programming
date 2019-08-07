73e498ac91c2e698ed853f60a6c17924ab390287
Google Google <Google@students.softuni.bg>
2019-08-07 10:06:17 +0300

    Initial

---
+++ Intillegio.Data/Data/IntillegioContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Intillegio.Data.Data
+{
+    public class IntillegioContext : IdentityDbContext
+    {
+        public IntillegioContext(DbContextOptions<IntillegioContext> options)
+            : base(options)
+        {
+        }
+    }
+}
